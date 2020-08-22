b6ada9888d54b3a22d0a311e211eb37aedcd37bb
zzp <847587146@qq.com>
2020-08-22 17:01:05 +0800

    a

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ app = flasker.create_app()
 # 这里临时注册蓝图
 app.register_blueprint(login_route.login, url_prefix='/login')
 
-app.run(host='127.0.0.1',port=8888,debug=True)
+app.run(host='0.0.0.0',port=8888,debug=True)
