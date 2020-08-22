61046eefa32975a787afaa2e4afca2547660effa
zheng <847587146@qq.com>
2020-08-22 17:12:54 +0800

    mereate

--- run.py
+++ run.py
@@ -1,12 +1,4 @@
 import flasker
 app = flasker.create_app()
 
-<<<<<<< HEAD
-app.run(host='127.0.0.1',port=8888,debug=True)
-=======
-
-# 这里临时注册蓝图
-app.register_blueprint(login_route.login, url_prefix='/login')
-
 app.run(host='0.0.0.0',port=8888,debug=True)
->>>>>>> b6ada9888d54b3a22d0a311e211eb37aedcd37bb
